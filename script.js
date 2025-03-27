ec914bf5bd6a21aba137b732070d9fa8cc8f5499
Adrian Kahiga <kahigaadrian7@gmail.com>
2025-03-27 07:10:57 +0300

    created function that displays recipies related to the ingredient once the search button in clicked

--- script.js
+++ script.js
@@ -5,3 +5,33 @@ const mealDetailsContent = document.querySelector('.meal-details-content');
 
 searchBtn.addEventListener('click', getMealList);
 mealList.addEventListener('click', getMealRecipe);
+
+function getMealList(){
+    let searchInputTxt = document.getElementById('search-input').value; 
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`) 
+    .then(response => response.json())   
+    .then(data => {                      
+        let html = "";
+        if(data.meals){
+            data.meals.forEach(meal => {
+                html += `
+                    <div class = "meal-item" data-id = "${meal.idMeal}">    
+                    <div class = "meal-img">
+                    <img src = "${meal.strMealThumb}" alt = "food">
+                    </div>
+                    <div class = "meal-name">
+                    <h3>${meal.strMeal}</h3>
+                    <a href = "#" class = "recipe-btn">Get Recipe</a>
+                    </div>
+                    </div>
+                `;
+            });
+            mealList.classList.remove('notFound');
+        } else{
+            html = "Sorry, we didn't find any meal!";
+            mealList.classList.add('notFound');
+        }
+
+        mealList.innerHTML = html;
+    });
+}
