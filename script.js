729f9b0674ef9a2d6d9b8519be912e87f1bbe523
Adrian Kahiga <kahigaadrian7@gmail.com>
2025-03-27 07:18:47 +0300

    created funtion that displays recipies once recipe button is clicked

--- script.js
+++ script.js
@@ -35,3 +35,13 @@ function getMealList(){
         mealList.innerHTML = html;
     });
 }
+
+function getMealRecipe(e){
+    e.preventDefault();
+    if(e.target.classList.contains('recipe-btn')){
+        let mealItem = e.target.parentElement.parentElement;
+        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`)
+        .then(response => response.json())
+        .then(data => mealRecipeModal(data.meals)); 
+    }
+}
